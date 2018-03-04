38f41c2c503138164f0da60842f92248c4495fd6
Roman <roman.makovyak@relayr.io>
2018-03-05 00:55:34 +0100

    add firebase authorization

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+
   render() {
     return (
       <div className="App">
@@ -11,8 +14,15 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Welcome, {this.props.name}
         </p>
+        <button onClick={() => {
+          firebase.auth().signOut().then(function () {
+            // Sign-out successful.
+          }).catch(function (error) {
+            // An error happened.
+          });
+        }}>sign out</button>
       </div>
     );
   }
